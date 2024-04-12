7d73ad6cec57a7b11fc846db1c11ef36f39814a1
SinghJigyasa <jig86026@gmail.com>
2024-04-13 00:57:34 +0530

    ui changes

--- src/component/HomePage.js
+++ src/component/HomePage.js
@@ -5,25 +5,69 @@ import React, { useEffect, useState } from 'react'
 
 function MovieListComp(){
     const [movieList, SetMovieList] = useState([])
+    const [searchKey,setSearchKey] = useState('')
+    const handleChange=(e)=>{
+        setSearchKey(e.target.value)
+  }
 
     useEffect(()=>{
         getMovieList()
-    },[])
+    },[searchKey])
     const getMovieList=()=>{
-        axios.get(`http://www.omdbapi.com/?s=kabhi&apikey=aab08961`)
+        axios.get(`http://www.omdbapi.com/?s=${searchKey}&apikey=aab08961`)
         .then(response=>{
             SetMovieList(response.data)
+            
         })
+        console.log(movieList?.Response,"console.log(response.data.Response);");
     }
-    console.log(movieList?.Search)
     return(
         <div >
-           {movieList?.Search && movieList.Search.map((item,idx) =>
-            <div key={idx} className='d-flex justify-content-start m-2' >
-               <img src={item.Poster} alt='movie' width={350} />
-            </div>
-           )}
-        </div>
+             <ul className="nav nav-pills flex-column mt-4 m-2 ">
+            <li className="input-group mb-4">
+              <span className="input-group-text">
+                 <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-search"
+                  viewBox="0 0 16 16"
+                 >
+                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                </svg>
+              </span>
+              <input onChange={(e)=>handleChange(e)}
+                className="form-control"
+                type="search"
+                placeholder="search..."
+              />
+            </li>
+            </ul>  
+          {
+            movieList?.Response ?
+            movieList?.Search && movieList.Search.map((item,idx) =>
+           
+                //            <div class="card" style="width: 18rem;">
+                //   <img class="card-img-top" src="..." alt="Card image cap">
+                //   <div class="card-body">
+                //     <h5 class="card-title">Card title</h5>
+                //     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                //     <a href="#" class="btn btn-primary">Go somewhere</a>
+                //   </div>
+                // </div>
+                        <div key={idx} style={{ display: 'flex', justifyContent: 'start', margin: '2px', width: '18rem' }}>
+                            <div className='card'>
+                                <img className='card-img-top' src={item.Poster} alt='movie' width={350} />
+                            <div className='card-body'>
+                                <h6 className='card-title'>{item.Title}</h6>
+                            </div>
+                            </div>
+                        </div>
+                        ):
+                        <h1>No Data</h1>
+          }
+    </div>
     )
 }
 export default MovieListComp;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Wishlist from './component/wishlist';
+import Login from './component/login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
        <Routes>
-         <Route path='/' element={ <App />}/>
+         <Route path='/' element={ <Login />}/>
+         <Route path='/home' element={<App/>}/>
          <Route path='/watchlist' element={<Wishlist/>}/>
        </Routes>
     </BrowserRouter>
